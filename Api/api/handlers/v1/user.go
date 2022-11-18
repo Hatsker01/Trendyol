06de3503aeecaa67942f8eb272f4cb3d568378a0
Jamshidbek <jamshidbek1805@gmail.com>
2022-11-18 22:40:05 +0500

    some remove last api

--- Api/api/handlers/v1/user.go
+++ Api/api/handlers/v1/user.go
@@ -17,8 +17,8 @@ import (
 // @Tags user
 // @Accept json
 // @Produce json
-// @Param user request body model.CreateUser true "UserCreate"
-// @Success 200 {object} model.User
+// @Param user request body CreateUser true "UserCreate"
+// @Success 200 {object} CreateUser
 // @Success 400 {object} response
 // @Success 500 {object} response
 // @Router   /v1/users/ [POST]
@@ -44,4 +44,17 @@ func (h *handlerV1) CreateUser(c *gin.Context) {
 		h.log.Error("failed while creating user json",logger.Error(err))
 	}
 	c.JSON(http.StatusCreated, user)
+}
+type CreateUser struct {
+	id         string `json:"id"`
+	first_name string `json:"first_name" binding:"required"`
+	last_name  string `json:"last_name" binding:"required"`
+	username   string `json:"username" binding:"required,min=4"`
+	phone      string `json:"phone" binding:"required,min=5"`
+	email      string `json:"email" binding:"required,email"`
+	password   string `json:"password" binding:"required,min=5"`
+	address    string `json:"address" binding:"required,min=7"`
+	gender     string `json:"gender" binding:"required"`
+	role       string `json:"role" binding:"required"`
+	postalcode string `json:"postalcode" binding:"required"`
 }
--- api/cmd/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/Trendyol/api/internal/app"
-)
-
-func main() {
-	app.Run()
-}
